e51957e1659d3198e0ebb8b02ef2939b1c50ab1e
thein3rovert <danielolaibi@gmail.com>
2026-02-08 11:39:03 +0000

    chore: fix ff and nvim tree cmd

--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -34,6 +34,7 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
