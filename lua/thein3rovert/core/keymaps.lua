7a82eec7d4d388a32897a80f0cfb9406140ce5aa
thein3rovert <danielolaibi@gmail.com>
2026-02-20 15:05:07 +0000

    chore: update close highlight

--- lua/thein3rovert/core/keymaps.lua
+++ lua/thein3rovert/core/keymaps.lua
@@ -6,6 +6,9 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- clear search highlights when pressing escape
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
