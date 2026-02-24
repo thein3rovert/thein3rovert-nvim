c2c034e2e7451573431c544622527c4851b498a9
thein3rovert <danielolaibi@gmail.com>
2026-02-24 09:49:42 +0000

    chore: update keymap and remove few plugins

--- lua/thein3rovert/plugins/which-key.lua
+++ lua/thein3rovert/plugins/which-key.lua
@@ -1,13 +1,27 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
+
+  -- init = function()
+  --   vim.o.timeout = true
+  --   vim.o.timeoutlen = 500
+  -- end,
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    preset = "helix",
+    icons = {
+      mappings = false,
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
   },
 }
