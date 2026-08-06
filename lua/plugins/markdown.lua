b328dffa6f002b678849064ad7677291fe1133e9
thein3rovert <danielolaibi@gmail.com>
2026-08-06 22:57:06 +0100

    feat: use simpler render-markdown config with icon symbols from main
    - Replace plain text heading icons (# ##) with symbols (󰲡 󰲣 󰲥)
    - Simplify config: remove LaTeX, LSP completions, conceallevel features
    - Set code blocks to 'block' width with right padding
    - Only load for markdown files (remove copilot-chat)
    - Comment out experimental config for easy revert

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,51 +1,71 @@
+-- Main branch config (simpler, with icon symbols)
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  enabled = true,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  ft = { "markdown" },
   opts = {
-    render_modes = true,
-
-    completions = {
-      lsp = {
-        enabled = true,
-      },
-    },
-
-    latex = {
-      enabled = true,
-    },
-
-    heading = {
-      atx = false,
-      icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
-    },
-
-    bullet = {
-      icons = { "-", "-", "-", "-" },
-    },
-
+    file_types = { "markdown" },
     code = {
-      conceal_delimiters = true,
+      sign = false,
+      width = "block",
+      right_pad = 1,
     },
-
-    anti_conceal = {
-      enabled = false,
-    },
-
-    sign = {
-      enabled = false,
-    },
-
-    win_options = {
-      conceallevel = { default = vim.o.conceallevel, rendered = 3 },
-    },
-
-    link = {
-      enabled = true,
+    heading = {
+      sign = false,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
     },
   },
-  ft = { "copilot-chat", "markdown" },
 }
+
+-- Experimental config (commented out - has more features like LaTeX, LSP completions)
+-- return {
+--   "MeanderingProgrammer/render-markdown.nvim",
+--   enabled = true,
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-tree/nvim-web-devicons",
+--   },
+--   opts = {
+--     render_modes = true,
+--
+--     completions = {
+--       lsp = {
+--         enabled = true,
+--       },
+--     },
+--
+--     latex = {
+--       enabled = true,
+--     },
+--
+--     heading = {
+--       atx = false,
+--       icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+--     },
+--
+--     bullet = {
+--       icons = { "-", "-", "-", "-" },
+--     },
+--
+--     code = {
+--       conceal_delimiters = true,
+--     },
+--
+--     anti_conceal = {
+--       enabled = false,
+--     },
+--
+--     sign = {
+--       enabled = false,
+--     },
+--
+--     win_options = {
+--       conceallevel = { default = vim.o.conceallevel, rendered = 3 },
+--     },
+--
+--     link = {
+--       enabled = true,
+--     },
+--   },
+--   ft = { "copilot-chat", "markdown" },
+-- }
